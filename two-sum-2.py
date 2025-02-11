96349ba174b71855f8841cb0fff1fa823e2aa8b3
Sudhir Hanumanthappa <sudhir.hanumanthappa@accenture.com>
2025-02-11 17:07:45 +0530

    alt solution no. 2 to two-sum problem

---
+++ two-sum-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def twoSum(self, nums, target):
+        hashtable = {}
+        for i in range(len(nums)):
+            x = target - nums[i]
+            if x in hashtable:
+                return [hashtable[x], i]
+            hashtable[nums[i]] = i
+
+tests = [
+    (
+        ([2, 7, 11, 15], 9),
+        [0, 1],
+    ),
+    (
+        ([3, 2, 4], 6),
+        [1, 2],
+    ),
+   (
+       ([3, 3], 6),
+       [0, 1],
+   ),
+]
