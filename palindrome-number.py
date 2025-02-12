c35381ecdcbb4e3589b929fe9c9ad406a9444e93
Sudhir Hanumanthappa <sudhir.hanumanthappa@accenture.com>
2025-02-12 18:25:38 +0530

    day-2: solution for Palindrome number

---
+++ palindrome-number.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # converting number to string to enable reversing of the number
+        if x < 0:
+            return False
+
+        y = str(x)
+        y = int(y[::-1])
+        if x == y:
+            return True
+        else:
+            return False
+
+
+tests = [
+    (
+        (121,),
+        True,
+    ),
+    (
+        (10,),
+        False,
+    ),
+    (
+        (-121,),
+        False,
+    ),
+]
